e4ace653ba1d51f407b4cdfd5593185bae532414
GIS <dev@gis4biz.ru>
2023-08-03 10:18:50 +0300

    Initial commit

---
+++ src/ProductImportServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Notabenedev\ProductImport;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProductImportServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
